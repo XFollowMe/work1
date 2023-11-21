3cba9d4b8d4dd901bb7cc62d04eae0f430bc4702
XFollowMe <148006321+XFollowMe@users.noreply.github.com>
2023-11-21 21:59:30 +0300

    lab3

--- 2.py
+++ 2.py
@@ -1,21 +1,15 @@
-import csv
-import json
+# TODO Напишите функцию find_common_participants
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
 
-INPUT_FILENAME = "input.csv"
-OUTPUT_FILENAME = "output.json"
+def find_common_participants(people_one,people_two, delimiter = ","):
+    people1 = people_one.split(delimiter)
+    people2 = people_two.split(delimiter)
 
+    people3 = list(set(people1).intersection(people2))
+    participiants_list = sorted(people3)
 
-def task() -> None:
-    with open(INPUT_FILENAME, "r") as f:
-        file = [string for string in csv.DictReader(f)]
+    return participiants_list
+# TODO Провеьте работу функции с разделителем отличным от запятой
 
-    with open(OUTPUT_FILENAME, "w") as r:
-        r.write(json.dumps(file, indent=4))
-
-
-if __name__ == '__main__':
-    task()
-
-    with open(OUTPUT_FILENAME) as output_f:
-        for line in output_f:
-          print(line, end="")
+print(find_common_participants(participants_first_group,participants_second_group))
