db9e62e6ea8f1fd9eb0e4f6d16967bcd7bf22239
XFollowMe <148006321+XFollowMe@users.noreply.github.com>
2023-11-21 22:09:16 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,11 +1,15 @@
-import json
 
-file = "input.json"
+def find_index(items, item):
+    for index, i in enumerate(items):
+        if i == item:
+            return index
 
-def task() -> float:
-    with open(file,"r") as a:
-        text = json.load(a)
 
-        sums = sum([item["score"] * item["weight"] for item in text])
-        return (f"{sums:.3f}")
-print(task())
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item =find_index(items_list,find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
