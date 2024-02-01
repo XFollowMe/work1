4940e8fa6225d33cdb50a2b7babd73653ec49624
XFollowMe <148006321+XFollowMe@users.noreply.github.com>
2024-02-01 20:53:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        self._name = name
+        self._author = author
+        if not isinstance(pages,int):
+            raise TypeError
+        if pages < 6:
+            raise ValueError
+        self.pages = pages
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        self._name = name
+        self._author = author
+        if not isinstance(duration,float):
+            raise TypeError
+        if duration <= 1:
+            raise ValueError
+        self.duration = duration
